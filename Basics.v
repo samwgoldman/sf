e3e78cee6563d604a594fa51ba987efe9b732bc7
Sam Goldman <samwgoldman@gmail.com>
2013-09-16 22:10:52 -1000

    Basics: plus_id_exercise

--- Basics.v
+++ Basics.v
@@ -707,7 +707,11 @@ Proof.
 Theorem plus_id_exercise : forall n m o : nat,
   n = m -> m = o -> n + m = m + o.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n m o H1 H2.
+  rewrite -> H1.
+  rewrite -> H2.
+  reflexivity.
+  Qed.
 (** [] *)
 
 (** As we've seen in earlier examples, the [Admitted] command
