77215061c8dafcc243c3cf6e1db421d1790d5898
Sam Goldman <samwgoldman@gmail.com>
2013-09-16 22:16:18 -1000

    Basics: zero_nbeq_plus_1

--- Basics.v
+++ Basics.v
@@ -831,7 +831,10 @@ Proof.
 Theorem zero_nbeq_plus_1 : forall n : nat,
   beq_nat 0 (n + 1) = false.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n. destruct n as [| n'].
+  reflexivity.
+  reflexivity.
+  Qed.
 (** [] *)
 
 (* ###################################################################### *)
