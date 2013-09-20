65c80be06c674d1fade8b6a046f6873ad7f2e9f3
Sam Goldman <samwgoldman@gmail.com>
2013-09-19 22:12:27 -1000

    Induction: plus_swap'

--- Induction.v
+++ Induction.v
@@ -596,7 +596,15 @@ Proof.
 Theorem plus_swap' : forall n m p : nat, 
   n + (m + p) = m + (n + p).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n m p.
+  rewrite -> plus_assoc.
+  rewrite -> plus_assoc.
+  replace (n + m) with (m + n).
+  reflexivity.
+  rewrite -> plus_comm.
+  reflexivity.
+  Qed.
+
 (** [] *)
 
 
