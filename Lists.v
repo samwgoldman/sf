2a2d71d8edf13f9247c4d07cca8fb5339a181d58
Sam Goldman <samwgoldman@gmail.com>
2014-01-18 22:56:50 -0800

    Lists: bag_proofs

--- Lists.v
+++ Lists.v
@@ -883,7 +883,12 @@ Proof.
 Theorem count_member_nonzero : forall (s : bag),
   ble_nat 1 (count 1 (1 :: s)) = true.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros s. destruct s as [| n s'].
+  Case "s = nil".
+    reflexivity.
+  Case "s = n : s'".
+    reflexivity.
+  Qed.
 
 (** The following lemma about [ble_nat] might help you in the next proof. *)
 
@@ -899,7 +904,15 @@ Proof.
 Theorem remove_decreases_count: forall (s : bag),
   ble_nat (count 0 (remove_one 0 s)) (count 0 s) = true.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros s. induction s as [| n s'].
+  Case "s = nil".
+    reflexivity.
+  Case "s = n :: s'".
+    simpl.
+    destruct n as [| n'].
+    simpl. rewrite -> ble_n_Sn. reflexivity.
+    simpl. rewrite -> IHs'. reflexivity.
+  Qed.
 (** [] *)
 
 (** **** Exercise: 3 stars, optional (bag_count_sum) *)  
