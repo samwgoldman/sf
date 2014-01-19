48f80bc948d0f9a4326f977b8a0635957d67e537
Sam Goldman <samwgoldman@gmail.com>
2014-01-18 20:30:14 -0800

    Lists: snd_fst_is_swap

--- Lists.v
+++ Lists.v
@@ -103,7 +103,7 @@ Proof.
 Theorem snd_fst_is_swap : forall (p : natprod),
   (snd p, fst p) = swap_pair p.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros p. destruct p as [n m]. simpl. reflexivity. Qed.
 (** [] *)
 
 (** **** Exercise: 1 star, optional (fst_swap_is_snd) *)
