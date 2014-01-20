1494950917e00b4b217a13a6660b780277eb424f
Sam Goldman <samwgoldman@gmail.com>
2014-01-20 10:45:30 -1000

    Poly: map_rev

--- Poly.v
+++ Poly.v
@@ -839,11 +839,25 @@ Proof. reflexivity.  Qed.
 (** Show that [map] and [rev] commute.  You may need to define an
     auxiliary lemma. *)
 
+Lemma map_snoc : forall (X Y : Type) (f : X -> Y) (l : list X) (x : X),
+  map f (snoc l x) = snoc (map f l) (f x).
+Proof.
+  intros. induction l as [| h l'].
+  Case "l = nil".
+    reflexivity.
+  Case "l = h :: l'".
+    simpl. rewrite -> IHl'. reflexivity.
+  Qed.
 
 Theorem map_rev : forall (X Y : Type) (f : X -> Y) (l : list X),
   map f (rev l) = rev (map f l).
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros X Y f l. induction l as [| x l'].
+  Case "l = nil".
+    reflexivity.
+  Case "l = x :: l'".
+    simpl. rewrite -> map_snoc. rewrite -> IHl'. reflexivity.
+  Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars (flat_map) *)
